712617b30bc64db488c6eda32f628888f57a64d2
Professionallance1stt <professionallance1st@gmail.com>
2022-02-15 17:44:37 -0500

    NO WARNINGS

--- app/src/main/java/com/example/mylogin/Categories.kt
+++ app/src/main/java/com/example/mylogin/Categories.kt
@@ -15,22 +15,21 @@ class Categories : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_categories)
-        var coupList: MutableList<Coupons> = ArrayList()
-        var catList: MutableList<String> = ArrayList()
+        val coupList: MutableList<Coupons> = ArrayList()
+        val catList: MutableList<String> = ArrayList()
         var line: String?
-        var lines = ""
         val minput = InputStreamReader(assets.open("tessts.csv"))
         val reader = BufferedReader(minput)
-        var displayData: String = ""
+        var displayData = ""
         while (reader.readLine().also { line = it } != null) {
             val row: List<String> = line!!.split(",")
-            var number = 0;
+            var number = 0
             displayData =
                 displayData + row[0] + "\t" + row[1] + "\t" + row[2] + "\t" + row[3] + "\t" + row[4] + "\t" + row[5] + "\t" + row[6] + "\t" + row[7] + "\t" + row[8] + "\t" + row[9] + "\t" + row[10] + "\t" + row[11] + "\t" + row[12] + "\t" + row[13] + "\t" + row[14] + "\t" + row[15] + "\t" + row[16] + "\t" + row[17] + "\n"
             val values = line!!.split(",").toTypedArray()
             for (index in values) {
 
-                var coupon = Coupons()
+                val coupon = Coupons()
                 coupon.setCoupon(
                     row[0],
                     row[1],
@@ -55,16 +54,16 @@ class Categories : AppCompatActivity() {
             }
         }
         for (coupon in 0 until coupList.size) {
-            val ll_main = findViewById<LinearLayout>(R.id.ll_main_layout)
-            val button_dynamic = Button(this)
-            button_dynamic.layoutParams = LinearLayout.LayoutParams(
+            val llmain = findViewById<LinearLayout>(R.id.ll_main_layout)
+            val buttondynamic = Button(this)
+            buttondynamic.layoutParams = LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.WRAP_CONTENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT
             )
-            button_dynamic.text = coupList[coupon].id
-            button_dynamic.tag = coupList[coupon].id
+            buttondynamic.text = coupList[coupon].id
+            buttondynamic.tag = coupList[coupon].id
 
-            ll_main.addView(button_dynamic)
+            llmain.addView(buttondynamic)
         }
 
     }
