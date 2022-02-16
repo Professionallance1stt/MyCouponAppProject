fb1722f7a4cdbceb38ad4a6cab6f8cc357fd57c6
Professionallance1stt <professionallance1st@gmail.com>
2022-02-15 20:01:09 -0500

    This includes the comments

--- app/src/main/java/com/example/mylogin/Categories.kt
+++ app/src/main/java/com/example/mylogin/Categories.kt
@@ -24,11 +24,12 @@ class Categories : AppCompatActivity() {
         while (reader.readLine().also { line = it } != null) {
             val row: List<String> = line!!.split(",")
             var number = 0
+            //Creates a row system
             displayData =
                 displayData + row[0] + "\t" + row[1] + "\t" + row[2] + "\t" + row[3] + "\t" + row[4] + "\t" + row[5] + "\t" + row[6] + "\t" + row[7] + "\t" + row[8] + "\t" + row[9] + "\t" + row[10] + "\t" + row[11] + "\t" + row[12] + "\t" + row[13] + "\t" + row[14] + "\t" + row[15] + "\t" + row[16] + "\t" + row[17] + "\n"
             val values = line!!.split(",").toTypedArray()
             for (index in values) {
-
+//Rows changed to input into the list.
                 val coupon = Coupons()
                 coupon.setCoupon(
                     row[0],
@@ -45,6 +46,7 @@ class Categories : AppCompatActivity() {
                     row[11],
                     row[12], row[13], row[14], row[15], row[16], row[17]
                 )
+                //due to bug it will duplicate it 10 times over, with this it does not
                 number += 1
                 if (number % 10 == 0) {
                     catList.add(row[8])
@@ -53,6 +55,7 @@ class Categories : AppCompatActivity() {
 
             }
         }
+        //Adds the buttons to the layout
         for (coupon in 0 until coupList.size) {
             val llmain = findViewById<LinearLayout>(R.id.ll_main_layout)
             val buttondynamic = Button(this)
@@ -68,3 +71,5 @@ class Categories : AppCompatActivity() {
 
     }
 }
+
+
--- app/src/main/java/com/example/mylogin/MainActivity.java
+++ app/src/main/java/com/example/mylogin/MainActivity.java
@@ -21,6 +21,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
     // They have to be same name that it is on activity_main.xml
 private TextView register;
 private EditText email , password;
