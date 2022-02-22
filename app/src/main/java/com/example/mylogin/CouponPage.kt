b4d3b0a2ee004837b451eea958d77dbff60809a1
Professionallance1stt <professionallance1st@gmail.com>
2022-02-22 17:25:17 -0500

    The Coupon Page is finally done

--- app/src/main/java/com/example/mylogin/CouponPage.kt
+++ app/src/main/java/com/example/mylogin/CouponPage.kt
@@ -1,9 +1,15 @@
 package com.example.mylogin
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.WriterException
 
 
 class CouponPage : AppCompatActivity() {
@@ -11,7 +17,7 @@ class CouponPage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coupon_page)
 
-        var coupLists = CouponList()
+        val coupLists = CouponList()
         coupLists.coupList
 
         val couponName = findViewById<TextView>(R.id.txtCoupon)
@@ -21,7 +27,30 @@ class CouponPage : AppCompatActivity() {
         val desc = findViewById<TextView>(R.id.textDesc)
         desc.text =  intent.getStringExtra("Description")
         val barCode = findViewById<ImageView>(R.id.barCode)
-
+            barCode.setImageBitmap(intent.getStringExtra("CODE")?.let { generateBarCode(it) })
+    }
+    fun generateBarCode(text: String): Bitmap {
+        val width = 500
+        val height = 150
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val codeWriter = MultiFormatWriter()
+        try {
+            val bitMatrix = codeWriter.encode(
+                text,
+                BarcodeFormat.CODE_128,
+                width,
+                height
+            )
+            for (x in 0 until width) {
+                for (y in 0 until height) {
+                    val color = if (bitMatrix[x, y]) Color.BLACK else Color.WHITE
+                    bitmap.setPixel(x, y, color)
+                }
+            }
+        } catch (e: WriterException) {
+            Log.d("TAG", "generateBarCode: ${e.message}")
+        }
+        return bitmap
     }
 
 
