68531120ead45d29ab69d84853efcf4cf95376f1
Professionallance1stt <professionallance1st@gmail.com>
2022-02-22 13:25:37 -0500

    Adds CouponList.kt

---
+++ app/src/main/java/com/example/mylogin/CouponList.kt
@@ -0,0 +1,28 @@
+package com.example.mylogin
+
+class CouponList
+{
+    public var coupList: MutableList<Coupons> = ArrayList()
+    public var catList: MutableList<String> = ArrayList()
+    var indexer: Int? = null
+    private var single_instance: CouponList? = null
+    fun setIndex(_index: Int)
+    {
+        indexer = _index
+    }
+    fun getIndex(): Int?
+    {
+        return indexer
+    }
+    fun PopulateList(_coupon : Coupons)
+    {
+        coupList.add(_coupon)
+
+    }
+
+    fun getInstance(): CouponList? {
+        if (single_instance == null) single_instance = CouponList()
+        return single_instance
+    }
+
+}
